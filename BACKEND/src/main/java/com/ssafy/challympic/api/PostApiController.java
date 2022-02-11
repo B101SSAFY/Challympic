1ba35da4877f1db013775fbd328873191f921dbc
kwonohbeom <xpzmfhf@gmail.com>
2022-02-11 14:36:46 +0900

    feat<BE>: Personalize API test 코드 추가

--- BACKEND/src/main/java/com/ssafy/challympic/api/PostApiController.java
+++ BACKEND/src/main/java/com/ssafy/challympic/api/PostApiController.java
@@ -9,6 +9,12 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.personalizeruntime.PersonalizeRuntimeClient;
+import software.amazon.awssdk.services.personalizeruntime.model.GetPersonalizedRankingRequest;
+import software.amazon.awssdk.services.personalizeruntime.model.GetPersonalizedRankingResponse;
+import software.amazon.awssdk.services.personalizeruntime.model.PersonalizeRuntimeException;
+import software.amazon.awssdk.services.personalizeruntime.model.PredictedItem;
 
 import java.io.IOException;
 import java.util.*;
@@ -87,6 +93,61 @@ public class PostApiController {
 
         // 좋아요 수
         private Integer LikeCnt;
+    }
+
+    @GetMapping("/challenge/personalize/{userNo}")
+    public Result testPersonalize(@PathVariable("userNo") int userNo){
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    GetPersonalizedRanking <campaignArn> <userId> <items>\n\n" +
+                "Where:\n" +
+                "    campaignArn - The ARN of the campaign.\n" +
+                "    userId - The user ID to provide recommendations for.\n" +
+                "    itemList - A comma delimited list of items to rank for the user\n\n";
+
+        String campaignArn = "arn:aws:personalize:ap-northeast-2:362595014566:campaign/challympic-campaign2";
+        String userId = String.valueOf(userNo);
+        ArrayList<String> items = new ArrayList<>(Arrays.asList(userId.split("\\s*,\\s*")));
+        Region region = Region.US_EAST_1;
+        PersonalizeRuntimeClient personalizeRuntimeClient = PersonalizeRuntimeClient.builder()
+                .region(region)
+                .build();
+
+        getRankedRecs(personalizeRuntimeClient, campaignArn, userId, items);
+        personalizeRuntimeClient.close();
+
+        return new Result(true, HttpStatus.OK.value());
+    }
+
+    public static List<PredictedItem> getRankedRecs(PersonalizeRuntimeClient personalizeRuntimeClient,
+                                                    String campaignArn,
+                                                    String userId,
+                                                    ArrayList<String> items) {
+
+        try {
+            GetPersonalizedRankingRequest rankingRecommendationsRequest = GetPersonalizedRankingRequest.builder()
+                    .campaignArn(campaignArn)
+                    .userId(userId)
+                    .inputList(items)
+                    .build();
+
+            GetPersonalizedRankingResponse recommendationsResponse =
+                    personalizeRuntimeClient.getPersonalizedRanking(rankingRecommendationsRequest);
+            List<PredictedItem> rankedItems = recommendationsResponse.personalizedRanking();
+            int rank = 1;
+            for (PredictedItem item : rankedItems) {
+                System.out.println("Item ranked at position " + rank + " details");
+                System.out.println("Item Id is : " + item.itemId());
+                System.out.println("Item score is : " + item.score());
+                System.out.println("---------------------------------------------");
+                rank++;
+            }
+            return rankedItems;
+        } catch (PersonalizeRuntimeException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return null;
     }
 
     /**
---
+++ BACKEND/src/main/java/com/ssafy/challympic/util/Personalizer.java
@@ -0,0 +1,14 @@
+package com.ssafy.challympic.util;
+
+import software.amazon.awssdk.services.personalizeruntime.PersonalizeRuntimeClient;
+import software.amazon.awssdk.services.personalizeruntime.model.GetPersonalizedRankingRequest;
+import software.amazon.awssdk.services.personalizeruntime.model.GetPersonalizedRankingResponse;
+import software.amazon.awssdk.services.personalizeruntime.model.PersonalizeRuntimeException;
+import software.amazon.awssdk.services.personalizeruntime.model.PredictedItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Personalizer {
+
+}

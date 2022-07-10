198ff91aca7a2934f3b36bcf0b55d683a3e69960
곽현준 <kwak0568@naver.com>
2022-07-10 22:07:27 +0900

    chore<BE>: ActivityRepo 내 Transactional 수정

--- BACKEND/src/main/java/com/ssafy/challympic/repository/ActivityRepository.java
+++ BACKEND/src/main/java/com/ssafy/challympic/repository/ActivityRepository.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional(readOnly = true)
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
 
     List<Activity> findAllByUser_no(int userNo);
